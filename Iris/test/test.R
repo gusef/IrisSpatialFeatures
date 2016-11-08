4648c3d3b6869885fdb3e541ddc3354189e9554d
gusef <daniel_gusenleitner@dfci.harvard.edu>
2016-11-08 16:52:17 -0500

    Added test cases. raw testing files still missing.

--- Iris/test/test.R
+++ Iris/test/test.R
@@ -1,7 +1,8 @@
 setwd("C:/work/projects/imaging/Iris_repo/Iris/test")
 rm(list=ls())
 gc()
-
+require(Iris)
+require(RColorBrewer)
 
 raw_data <- Iris()
 raw_data<- read.raw(raw_data,
@@ -17,9 +18,25 @@ dataset <- threshold.dataset(raw_data,
 dataset <- threshold.dataset(dataset,
                              marker='PD-1 (Opal 540)',
                              marker_name='PD1',
-                             base=c('OTHER'))
+                             base=c('CD8+','OTHER'))
 
+#get the counts
+get.counts(dataset)
 
+#run the interaction analysis
+dataset <- extract.interactions(dataset)
+get.interactions(dataset,'CD8+ PD1+')
 
+#plotting interaction summaries
+plot.interactions(dataset,"SOX10+ PDL1+",xlim_fix=4)
 
+#plotting interaction maps
+int_markers <- c('CD8+ PD1+','SOX10+ PDL1+')
+int_marker_cols <- c('#dd1c77','#99d8c9')
+silent_markers <- c('CD8+ PD1-')
+silent_col=c('yellow')
+interaction.maps(dataset,int_markers,int_marker_cols,silent_markers,silent_col)
+
+#running the proximity / touching analysis
+touching <- extract_touches(dataset,raw=raw)
 
